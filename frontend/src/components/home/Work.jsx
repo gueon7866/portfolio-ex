998d3fa9a0a52ca97d4a6f213c1edf9878856ecf
gueon7866 <lsg7866@gmail.com>
2025-09-02 21:47:05 +0900

    1
    1

--- frontend/src/components/home/Work.jsx
+++ frontend/src/components/home/Work.jsx
@@ -6,22 +6,25 @@ const Work = () => {
     <div className='inner work-inner'>
       <h3 className='sub-tit-2'>works.</h3>
       <ul className="work-lst">
-        {works.map((work)=>(
+        {works.map((work) => (
           <li key={work.id}>
             <a href={work.links.notion} className='lst-t-wrap'>
-            <h4 className="lst-tit">
-              {work.title}
-            </h4>
-            <div className="tags">
-              {work.tags.map((t,i)=>(
-                <span key={i}>{t}</span>
-              ))}
-            </div>
+              <h4 className="lst-tit">
+                {work.title}
+              </h4>
+              <p className="lst-dsc">
+                {work.description}
+              </p>
+              <div className="tags">
+                {work.tags.map((t, i) => (
+                  <span key={i}>{t}</span>
+                ))}
+              </div>
             </a>
             <a href={work.links.demo} className="lst-img-wrap">
-              <div 
-              className="bg"
-              style={{backgroundImage:`url(${work.thumbnail})`}}
+              <div
+                className="bg"
+                style={{ backgroundImage: `url(${work.thumbnail})` }}
               ></div>
             </a>
           </li>
